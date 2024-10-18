fb9c492a004689abee1cee98bd61a5ce6bcce496
Jacob DiBiase <jakesback93@hotmail.com>
2024-10-18 12:49:25 -0700

    Add event listeners for buttons

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ let intervalId;
 let startTime;
 let updatedTime;
 
-function startStopwatch() {}
+function startStopwatch() {
+}
 
 function stopStopwatch() {}
 
@@ -12,3 +13,10 @@ function resetStopwatch() {}
 function updateTime() {}
 
 function formatTime() {}
+
+// Button click event listeners
+document.getElementById("startBtn").addEventListener("click", startStopwatch);
+
+document.getElementById("stopBtn").addEventListener("click", stopStopwatch);
+
+document.getElementById("resetBtn").addEventListener("click", resetStopwatch);
