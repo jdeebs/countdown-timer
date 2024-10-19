210c060691676c83501deb067f52e455723ae238
Jacob DiBiase <jakesback93@hotmail.com>
2024-10-19 12:56:47 -0700

    Remove milliseconds from timer, add resetTimer logic

--- index.js
+++ index.js
@@ -2,31 +2,42 @@ let startTime = 0;
 let elapsedTime = 0;
 let timerInterval;
 
+document.getElementById("timer").textContent = "00:00:00";
+
 function startTimer() {
+  if (startTime === 0) {
     startTime = Date.now();
-    timerInterval = setInterval(() => {
-        elapsedTime = Date.now() - startTime;
-        document.getElementById("timer").textContent = formatTime(elapsedTime);
-    }, 10); // Update every 10 milliseconds
+  }
+  
+  timerInterval = setInterval(() => {
+    elapsedTime = Date.now() - startTime;
+    document.getElementById("timer").textContent = formatTime(elapsedTime);
+  }, 10); // Update every 10 milliseconds
 }
 
 function stopTimer() {
-    clearInterval(timerInterval);
+  clearInterval(timerInterval);
 }
 
-function resetTimer() {}
+function resetTimer() {
+  stopTimer();
+  startTime = 0;
+  elapsedTime = 0;
+  document.getElementById("timer").textContent = "00:00:00";
+}
 
 function formatTime(time) {
-    const totalSeconds = Math.floor(time / 1000);
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = totalSeconds % 60;
-    const milliseconds = Math.floor(time / 10);
+  const totalSeconds = Math.floor(time / 1000);
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
 
-    // Add leading zeros
-    const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${milliseconds.toString()}`;
+  // Add leading zeros
+  const formattedTime = `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 
-    return formattedTime;
+  return formattedTime;
 }
 
 // Button click event listeners
